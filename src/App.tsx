e8c172155a467f1254525c4a8fa3cc6b54c18b5f
Vinicius Rodrigues Varela <rodigues.vv@gmail.com>
2023-09-03 18:30:27 -0300

    Creating components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
-    <div className='App'>
-        <h2>Ola mundo</h2>
+    <div className="App">
+      <h2>Finder persons on github</h2>
+      <Outlet />
     </div>
   );
 }
 
-export default App
+export default App;
